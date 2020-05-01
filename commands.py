bae61dae7ffa8bb1efeea787db006f1143bf50ca
V Likhosherstov <vl304@goedel.eng.cam.ac.uk>
2020-05-01 23:21:52 +0100

    Added Reptile commands

--- commands.py
+++ commands.py
@@ -3,30 +3,27 @@ all_h_params = []
 # Omniglot
 for n_layers in [3, 4]:
     for inner_iters in [5, 10]:
-        for mode in ['Reptile', 'STFOML']:
-            for prob in [0, 0.1, 0.2, 1]:
-                h_params = {
-                    "config.dataset": "omniglot",
-                    "config.shots": 1,
-                    "config.classes": 30,
-                    "config.inner_batch": 30,
-                    "config.inner_iters": inner_iters,
-                    "config.meta_step": 0.1,
-                    "config.meta_batch": 5,
-                    "config.meta_iters": 60000,
-                    "config.eval_batch": 10,
-                    "config.eval_iters": 50,
-                    "config.learning_rate": 0.005,
-                    "config.meta_step_final": 0,
-                    "config.checkpoint": "ckpt_o130_{0}_pr={1}_l={2}_ii={3}".format(mode, prob, n_layers, inner_iters),
-                    "config.mode": mode,
-                    "config.exact_prob": prob,
-                    "config.n_layers": n_layers,
-                    "config.clip_grads": (inner_iters == 10),
-                    "config.clip_grad_value": 0.1
-                }
-                all_h_params.append(h_params)
+        learning_rate in [0.005, 0.0005]:
+            h_params = {
+                "config.dataset": "omniglot",
+                "config.shots": 1,
+                "config.classes": 30,
+                "config.inner_batch": 30,
+                "config.inner_iters": inner_iters,
+                "config.meta_step": 0.0005/learning_rate,
+                "config.meta_batch": 5,
+                "config.meta_iters": 60000,
+                "config.eval_batch": 10,
+                "config.eval_iters": 50,
+                "config.learning_rate": learning_rate,
+                "config.meta_step_final": 0,
+                "config.checkpoint": "ckpt_o130_Reptile_l={0}_ii={1}_lr={2}".format(n_layers, inner_iters, learning_rate),
+                "config.mode": 'Reptile',
+                "config.n_layers": n_layers
+            }
+            all_h_params.append(h_params)
 
+'''
 # Mini-ImageNet.
 for learning_rate in [0.001, 0.01]:
     for clip_value in [0.1, 1.0, 10.0]:
@@ -52,3 +49,4 @@ for learning_rate in [0.001, 0.01]:
                 "config.clip_grad_value": clip_value
             }
             all_h_params.append(h_params)
+'''
