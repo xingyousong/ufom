ca802039f17fb4fb72f0b990e256c24624e83e2b
V Likhosherstov <vl304@goedel.eng.cam.ac.uk>
2020-05-02 23:05:13 +0100

    Fixed reptile commands and added o120 commands

--- commands.py
+++ commands.py
@@ -1,27 +1,56 @@
 all_h_params = []
 
-# Omniglot
+# Omniglot Reptile
+for n_layers in [3, 4]:
+    for n_classes in [20, 30]:
+        for inner_iters in [5, 10]:
+            learning_rate in [0.005, 0.0005]:
+                h_params = {
+                    "config.dataset": "omniglot",
+                    "config.shots": 1,
+                    "config.classes": n_classes,
+                    "config.inner_batch": n_classes,
+                    "config.inner_iters": inner_iters,
+                    "config.meta_step": 0.0005/learning_rate,
+                    "config.meta_batch": 5,
+                    "config.meta_iters": 60000,
+                    "config.eval_batch": 10,
+                    "config.eval_iters": 50,
+                    "config.learning_rate": learning_rate,
+                    "config.meta_step_final": 0,
+                    "config.train_shots": inner_iters,
+                    "config.checkpoint": "ckpt_o1{0}_Reptile_l={1}_ii={2}_lr={3}".format(n_clases, n_layers, inner_iters, learning_rate),
+                    "config.mode": 'Reptile',
+                    "config.n_layers": n_layers
+                }
+                all_h_params.append(h_params)
+
+# Omniglot 1-shot-20-way
 for n_layers in [3, 4]:
     for inner_iters in [5, 10]:
         learning_rate in [0.005, 0.0005]:
-            h_params = {
-                "config.dataset": "omniglot",
-                "config.shots": 1,
-                "config.classes": 30,
-                "config.inner_batch": 30,
-                "config.inner_iters": inner_iters,
-                "config.meta_step": 0.0005/learning_rate,
-                "config.meta_batch": 5,
-                "config.meta_iters": 60000,
-                "config.eval_batch": 10,
-                "config.eval_iters": 50,
-                "config.learning_rate": learning_rate,
-                "config.meta_step_final": 0,
-                "config.checkpoint": "ckpt_o130_Reptile_l={0}_ii={1}_lr={2}".format(n_layers, inner_iters, learning_rate),
-                "config.mode": 'Reptile',
-                "config.n_layers": n_layers
-            }
-            all_h_params.append(h_params)
+            for prob in [0, 0.1, 0.2, 1]:
+                h_params = {
+                    "config.dataset": "omniglot",
+                    "config.shots": 1,
+                    "config.classes": 20,
+                    "config.inner_batch": 20,
+                    "config.inner_iters": inner_iters,
+                    "config.meta_step": 0.0005/learning_rate,
+                    "config.meta_batch": 5,
+                    "config.meta_iters": 60000,
+                    "config.eval_batch": 10,
+                    "config.eval_iters": 50,
+                    "config.learning_rate": learning_rate,
+                    "config.meta_step_final": 0,
+                    "config.checkpoint": "ckpt_o120_FOML_prob={0}_l={1}_ii={2}_lr={3}".format(prob, n_layers, inner_iters, learning_rate),
+                    "config.mode": 'FOML',
+                    "config.n_layers": n_layers,
+                    "config.exact_prob": prob,
+                    "config.clip_grads": True,
+                    "config.clip_grad_value": 0.1
+                }
+                all_h_params.append(h_params)
 
 '''
 # Mini-ImageNet.
